75b1b19d1cb7f8908955f2bf1a4da2b5ca1e16b7
Blake Howell <blake@evertrue.com>
2018-03-28 09:46:15 -0400

    Bump to v0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          ''
 description      'Installs/Configures aws_ecs-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'aufs'
 depends 'iptables', '~> 4.2.0'
