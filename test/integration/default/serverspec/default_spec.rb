918e77b0308f1b2676425a9d5e5acaa645260c3f
Adam Daines <adam.daines@tracsis.com>
2018-10-03 16:54:33 +0100

    Removed old test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -12,14 +12,3 @@ describe 'ECS' do
   end
 end
 
-context 'Configuration' do
-   describe file '/etc/ecs/ecs.config' do
-      it { is_expected.to be_file }
-      describe '#content' do
-         subject { super().content }
-         it { is_expected.to contain('ECS_DATADIR=/datadir') }
-         it { is_expected.to contain('ECS_CLUSTER=test') }
-         it { is_expected.to contain('ECS_LOGFILE=/log/ecs-agent.log')}
-      end
-   end
-end
